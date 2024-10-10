0af537500c42887964788027a343ba615f370ad4
Abijith26 <abimurugan.c@gmail.com>
2024-10-10 13:33:51 +0530

    Styled the elements using css

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,6 +9,8 @@ import { RouterView } from 'vue-router'
 
 <style scoped>
 * {
+  width: 100%;
+  height: 100%;
   padding: 0;
   margin: 0;
   box-sizing: border-box;
--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -1,13 +1,28 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+</script>
 <template>
-  <div>
-    <form>
+  <div class="container">
+    <form class="form-container" @submit.prevent="">
       <!-- Username Field -->
-      <label for="super-user-name">Name</label>
-      <input type="text" id="super-user-name" />
+      <div class="field-container">
+        <label for="super-user-name">Name</label>
+        <input
+          type="text"
+          id="super-user-name"
+          required
+          placeholder="Eg. Steve"
+          v-model="username"
+        />
+      </div>
       <!-- Password Field -->
-      <label for="super-user-name-password">Password</label>
-      <input type="password" id="super-user-name-password" />
+      <div class="field-container">
+        <label for="super-user-name-password">Password</label>
+        <input type="password" id="super-user-name-password" required v-model="password" />
+      </div>
     </form>
   </div>
 </template>
@@ -17,5 +32,49 @@
   padding: 0;
   margin: 0;
   box-sizing: border-box;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background: lightgrey;
+}
+.form-container {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  width: min-content;
+  height: min-content;
+  background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #ff53d4 60%, #62c2fe 90%);
+  padding: 70px;
+  border-radius: 5%;
+}
+
+.field-container {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  font-family: sans-serif;
+  font-weight: 600;
+}
+
+.field-container > input {
+  max-width: 350px;
+  height: 44px;
+  padding: 5px;
+  background-color: whitesmoke;
+  border-radius: 0.5rem;
+  padding: 0 1rem;
+  border: 2px solid transparent;
+  font-size: 1rem;
+}
+
+.field-container > input:hover,
+.field-container > input:focus {
+  outline: none;
+  border-color: #05060f;
 }
 </style>
